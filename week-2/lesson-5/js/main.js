d5a02a56f320ab5a14045143d628151c4e8ee996
Jordan Sheehan <jordansheehan@ufl.edu>
2022-10-15 15:17:03 -0400

    Lesson 5 completed.

---
+++ week-2/lesson-5/js/main.js
@@ -0,0 +1,25 @@
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+
+console.log(Math.round(3.5));
+
+console.log(Math.ceil(3.2));
+console.log(Math.floor(3.9));
+
+console.log(Math.pow(5, 2));
+
+console.log(Math.min(2, 0.5, 9));
+console.log(Math.max(2, 0.5, 9));
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+// Use floor instead of ceil because the random() method could generate 0
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
