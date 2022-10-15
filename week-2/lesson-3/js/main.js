f49f106eb66d4b74d66cf08fac9cd64013bf433d
Jordan Sheehan <jordansheehan@ufl.edu>
2022-10-15 15:05:37 -0400

    Lesson 3 completed.

---
+++ week-2/lesson-3/js/main.js
@@ -0,0 +1,24 @@
+const myVariable = "Mathematics";
+
+// Length property
+console.log(myVariable.length);
+
+// String methdos
+console.log(myVariable.charAt(6));
+
+console.log(myVariable.indexOf("ati"));
+
+console.log(myVariable.lastIndexOf("at"));
+
+console.log(myVariable.slice(5, 8));
+
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+
+console.log(myVariable.includes("mat"));
+
+console.log(myVariable.split("e")); // *** e is not included in the split
+
+console.log(myVariable.split(""));
+console.log("John,Joe,Dave".split(","));
+
