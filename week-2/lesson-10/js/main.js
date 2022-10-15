cb3b231f43c042e87bce68f5028822c40f51714c
Jordan Sheehan <jordansheehan@ufl.edu>
2022-10-15 16:11:08 -0400

    Lesson 10 completed.

---
+++ week-2/lesson-10/js/main.js
@@ -0,0 +1,15 @@
+// User input
+// alert("Hello, World!");
+
+// let myBoolean = confirm("Ok === True\nCancel === False");
+// console.log(myBoolean);
+
+let name = prompt("Please enter your name.");
+
+if (name) {
+  console.log(name.length);
+  console.log(name.trim().length);
+  console.log(name.trim());
+} else {
+  console.log("You didn't enter a name.");
+}
