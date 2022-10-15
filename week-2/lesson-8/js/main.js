bdb86dbad48502570b2006b2fbbd5663129ec79f
Jordan Sheehan <jordansheehan@ufl.edu>
2022-10-15 15:32:36 -0400

    Lesson 9 completed.

---
+++ week-2/lesson-8/js/main.js
@@ -0,0 +1,13 @@
+switch(Math.floor(Math.random() * 3 + 1)) {
+    case 1: 
+        console.log(1);
+        break;
+    case 2: 
+        console.log(2);
+        break;
+    case 3: 
+        console.log(3);
+        break;
+    default:
+        console.log("No match");
+}
