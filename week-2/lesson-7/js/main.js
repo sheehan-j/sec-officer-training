9f6055c41a735779f8587184db772dcc749b4b7c
Jordan Sheehan <jordansheehan@ufl.edu>
2022-10-15 15:29:26 -0400

    Lessson 7 completed.

--- week-2/lesson-7/js/main.js
+++ week-2/lesson-7/js/main.js
@@ -1,3 +1,17 @@
 const myName = "Jordan";
 
 console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+
+let soup = "chicken noodle soup";
+let reply;
+let isBanned = false;
+let crackers = true;
+
+if (isBanned) {
+    reply = "No soup";
+} else if (soup) {
+    reply = `Here's your order of ${soup}`;
+} else {
+    reply = `Sorry, we're out of ${soup}`;
+}
+console.log(reply);
